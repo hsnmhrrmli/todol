1e7e3f3e2b525e9c0a3828ca307822f689b0800a
hsnmhrrmli <hsnmhrrmli@gmail.com>
2023-02-03 05:19:51 +0400

    ...

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@ const add = document.getElementById('add')
 const text = document.getElementById('text')
 const ul = document.querySelector('ul')
 const li = document.createElement('li')
+const checkbox = document.createElement('button')
+checkbox.style.width="15px"
+checkbox.style.height="13px"
+checkbox.style.marginLeft="5px"
+checkbox.style.marginBottom="5px"
+// li.style.display="flex"
+// li.style.justifyContent="base-line"
 add.addEventListener('click', () => {
   // console.log(text.value)
     li.innerHTML = text.value
-    console.log(li)
-    text.value = ''
+    li.appendChild(checkbox)
+    ul.style.display="block"
+    // console.log(li)
     ul.appendChild(li)
+    text.value = ''
 })
